c10b73f4b6bc4a16abc654ed8e544d8564fdcc24
hamzaadil56 <hamzaadil56@gmail.com>
2023-05-08 21:50:42 +0500

    add dateOfBirth

--- giaic/src/lib/drizzle.ts
+++ giaic/src/lib/drizzle.ts
@@ -19,6 +19,7 @@ export const UsersTable = pgTable("applied_users", {
   city: text("city").notNull(),
   email: text("email").notNull(),
   gender: text("gender").notNull(),
+  dateOfBirth: date("birth_date").notNull(),
   highestQualification: text("highest_qualification").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
@@ -28,9 +29,6 @@ export const UsersTable = pgTable("applied_users", {
   programmingLanguages: text("programming_languages"),
 });
 
-// serial("user_id")
-//     .references(() => UsersTable.id)
-//     .notNull(),
 export const ExperiencesTable = pgTable("experiences", {
   id: serial("id").primaryKey(),
   userId: serial("user_id")

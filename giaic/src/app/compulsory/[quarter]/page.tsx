632eb634b18cb49d9080574b807acafee9146b79
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-05-08 13:22:55 +0500

    courses page design changed

--- giaic/src/app/compulsory/[quarter]/page.tsx
+++ giaic/src/app/compulsory/[quarter]/page.tsx
@@ -34,7 +34,7 @@ async function Page({ params }: { params: { quarter: string } }) {
         return (
             <div className='flex flex-col'>
                 <div className='flex justify-center items-center py-28'>
-                    <h1 className='text-2xl '>Track Not Found !</h1>
+                    <h1 className='text-2xl '>Course Not Found !</h1>
                 </div>
             </div>
         )
@@ -43,28 +43,28 @@ async function Page({ params }: { params: { quarter: string } }) {
     return (
 
         <>
-            <div className={`w-full mb-20 ${roboto.className}`}>
+            <div className={`w-full mb-20`}>
                 <div className='w-[1300px] xl:w-[90%] lg:w-[95%] m-auto flex md:block justify-between mt-20 gap-10 relative'>
                     <div className='w-[270px] md:w-full h-fit md:border border-zinc-800 md:rounded-lg flex-shrink-0 sticky md:relative md:top-0 top-40'>
-                        <div className='h-fit rounded-lg p-3 bg-zinc-300'>
-                            <h2 className='font-normal text-[#f1f1f1] text-sm leading-tight bg-[#045084] p-3 rounded-lg'>Compulsory Quarters</h2>
+                        <div className='h-fit rounded-lg p-3 bg-gray-50 '>
+                            <h2 className='font-normal text-[#f1f1f1] text-sm leading-tight bg-[#045084] p-3 rounded-lg'>Compulsory Courses</h2>
                             <ul className='mt-2 text-zinc-800'>
                                 {
                                     ['1', '2', '3'].map((val, index) => {
                                         return (
-                                            <Link key={index} href={`/compulsory/${encodeURIComponent(val)}`}><li className={`py-[5px] pl-5 text-sm tracking-widest border-l-[2px] ${params.quarter === val ? 'border-[#045084] text-[#045084]' : 'border-[#c2c2c2]'}`}>Quarter - {val}</li></Link>
+                                            <Link key={index} href={`/compulsory/${encodeURIComponent(val)}`}><li className={`py-[5px] pl-5 text-sm tracking-widest border-l-[2px] ${params.quarter === val ? 'border-[#045084] text-[#045084]' : 'border-[#c2c2c2]'}`}>Course - {val}</li></Link>
                                         )
                                     })
                                 }
                             </ul>
                         </div>
                         <div className='mt-8 md:p-2'>
-                            <h3 className='font-bold text-zinc-800 text-base leading-tight'>Available Tracks</h3>
+                            <h3 className='font-bold text-zinc-800 text-base leading-tight'>Advance Courses</h3>
                             <div className='flex flex-col gap-2 mt-3'>
                                 {
                                     tracks.map((val, index) => {
                                         return (
-                                            <Link key={index} href={`/tracks/${encodeURIComponent(val.id)}/4`} className='py-2 px-3 leading-none text-sm text-zinc-800 rounded-lg bg-zinc-300 transition-all hover:bg-[#045084] hover:text-white'>{val.name}</Link>
+                                            <Link key={index} href={`/tracks/${encodeURIComponent(val.id)}/4`} className='py-2 px-3 leading-none text-sm text-zinc-800 rounded-lg bg-gray-50 transition-all hover:bg-[#045084] hover:text-white'>{val.name}</Link>
                                         )
                                     })
                                 }

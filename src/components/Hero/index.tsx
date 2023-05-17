dd46fdd3ccf4466dc554fa33b5061a8fadb997b7
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-05-18 03:18:53 +0500

    bring my code back in dev

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,10 +1,8 @@
 import Image from 'next/image';
 import React from 'react';
-// import Lines from '../../public/assets/lines.svg'
 import { Poppins } from 'next/font/google';
 import Cover from '../../../public/cover.jpg'
 import Link from 'next/link';
-// import { Header } from '@/components';
 
 const poppins = Poppins({
     weight: ['300', '400', '500', '800', '900'],
@@ -13,23 +11,21 @@ const poppins = Poppins({
 
 function Hero() {
     return (
-        <div className={`w-full relative py-10 md:py-5 overflow-hidden bg-zinc-100 ${poppins.className}`}>
-            {/* <div className='absolute bottom-0 w-full h-full bg_radial_gradient -z-10'></div> */}
-            {/* <Image src={Lines} alt='lines' className='absolute -top-10 xl:top-0 opacity-80 w-full xl:h-full md:hidden -z-20' /> */}
-            <div className='w-[1300px] xl:w-[90%] lg:w-[95%] m-auto mt-10 flex md:block justify-between items-center relative'>
-                <div className='w-1/2 md:w-full '>
-                    <h1 className='text-6xl lg:text-5xl sm:text-[2rem] sm:leading-10 sm:text-center tracking-wider text-main font-extrabold'>Governor Sindh</h1>
-                    <h1 className='text-[2.5rem] lg:text-4xl sm:text-[1.5rem] sm:text-center leading-[3rem] sm:leading-[2rem]  text-sub font-semibold mt-5 tracking-wider'>Initiative for<br />Artificial Intelligence,<br />Web 3.0 & Metaverse</h1>
-                    <p className='text-2xl sm:text-[1.25rem] sm:text-center font-extrabold text-main my-5'>
+        <div className={`w-full relative md:py-10 py-5 overflow-hidden bg-zinc-100 ${poppins.className}`}>
+            <div className='xl:w-[1300px] lg:w-[90%] w-[95%] m-auto mt-10 md:flex block justify-between items-center relative'>
+                <div className='md:w-1/2 w-full'>
+                    <h1 className='lg:text-6xl sm:text-5xl text-[2rem] sm:leading-none leading-10 sm:text-left text-center tracking-wider text-main font-extrabold'>Governor Sindh</h1>
+                    <h1 className='lg:text-[2.5rem] sm:text-4xl text-[1.5rem] sm:text-left text-center sm:leading-[3rem] leading-[2rem]  text-sub font-semibold mt-5 tracking-wider'>Initiative for<br />Artificial Intelligence,<br />Web 3.0 & Metaverse</h1>
+                    <p className='sm:text-2xl text-[1.25rem] sm:text-left text-center font-extrabold text-main my-5'>
                         Earn up to $5,000 / month
                     </p>
-                    <p className='my-5 text-xl sm:text-[1.125rem] sm:text-center text-main tracking-wider w-[80%] sm:w-full'>
+                    <p className='my-5 sm:text-xl text-[1.125rem] sm:text-left text-center text-main tracking-wider sm:w-[80%] w-full'>
                         Opening the Governor House and Removing Barriers to Educate the Youth of Sindh.
                     </p>
-                    <Link href={'/apply'}><button className='mt-5 py-4 w-52 sm:py-3 sm:w-full sm:text-sm text-center tracking-widest bg-sub text-white text-base font-semibold transition-all hover:translate-y-1'>APPLY NOW</button></Link>
+                    <Link href={'/apply'}><button className='mt-5 sm:py-4 py-3 sm:w-52 w-full sm:text-base text-sm text-center tracking-widest bg-sub text-white font-semibold transition-all hover:translate-y-1'>APPLY NOW</button></Link>
                 </div>
-                <div className='w-1/2 md:w-full flex justify-end md:justify-center md:mt-7'>
-                    <Image src={Cover} alt='piaic' className='w-[80%] lg:w-[95%] md:w-[500px] sm:w-full' />
+                <div className='md:w-1/2 w-full flex md:justify-end justify-center md:m-auto mt-7'>
+                    <Image src={Cover} alt='piaic' className='lg:w-[80%] md:w-[95%] sm:w-[500px] w-full' />
                 </div>
             </div>
         </div>

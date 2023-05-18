c93ef61922afed410518cef3ff4940c20986cd3a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-05-18 14:14:48 +0500

    admit card started

--- src/app/admit-card/page.tsx
+++ src/app/admit-card/page.tsx
@@ -57,27 +57,38 @@ export default function Page() {
       >
         Download Admit Card
       </h1>
-      <AdmitCard data={data} />
+      {/* <AdmitCard data={data} /> */}
       <form
         className="-top-10 z-10 mx-4 my-10 w-full max-w-2xl rounded px-4 py-8 text-black shadow-lg md:mx-10 md:px-6"
         onSubmit={handleSubmit(onFormSubmit)}
       >
-        <Input
-          type="number"
-          id="cnic"
-          placeholder="CNIC or B-Form Number"
-          required={true}
-          register={register}
-          errors={errors}
-        />
-        <Input
-          type="number"
-          id="phoneNumber"
-          placeholder="Phone Number"
-          required={true}
-          register={register}
-          errors={errors}
-        />
+        <div className="border-2 border-red-500">
+          <div className="my-6">
+            <label
+              htmlFor="phoneNumber"
+              className="mb-6 mt-4 text-slate-700 md:text-xl"
+            >
+              Phone Number *
+            </label>
+            <input
+              type="phoneNumber"
+              id="phoneNumber"
+              className={`mb-2 mt-1 block h-12 w-full rounded border border-gray-400 bg-gray-100 p-3 ${
+                errors?.phoneNumber
+                  ? "border-red-400 ring-red-500"
+                  : "focus:border-sub focus:ring-sub"
+              } outline-none focus:ring-1 md:text-xl`}
+              placeholder=" phoneNumber"
+              {...register("phoneNumber", { valueAsDate: false })}
+            />
+            {errors?.phoneNumber && (
+              <p className="mb-4 text-red-400">
+                {errors?.phoneNumber?.message}
+              </p>
+            )}
+          </div>
+          <button className="border-2 border-green-500">Get OTP</button>
+        </div>
         <Input
           type="email"
           id="email"

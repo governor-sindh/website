0a117982eb600bc5540e209bdd048cba07aa717e
Ubaid <rubaid690@gmail.com>
2023-05-21 23:01:45 +0500

    Application Counter

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -1,5 +1,6 @@
-'use client'
-import { useEffect, useState } from 'react';
+"use client";
+import { useEffect, useState } from "react";
+import CountUp from "react-countup";
 const Counter = () => {
   const [Counter, setCounter] = useState(0);
   const [loader, setLoder] = useState(true);
@@ -9,19 +10,34 @@ const Counter = () => {
 
   const fetchCounterValue = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/counter');
+      const response = await fetch("http://localhost:3000/api/counter");
       const data = await response.json();
       await setCounter(data);
-      setLoder(false)
+      setLoder(false);
     } catch (error) {
-      console.error('Error:', error);
-      setLoder(false)
+      console.error("Error:", error);
+      setLoder(false);
     }
   };
   return (
-    <div className='w-40 text-center'>
-      <h1 className='sm:pt-4 pt-3 sm:w-52 w-full sm:text-3xl text-xl text-main text-center tracking-widest font-semibold'>{loader ? "Loading...": Counter}</h1>
-      <h1 className='sm:pt-1 sm:ml-4 sm:text-base text-[1.125rem] text-center text-main tracking-wider sm:w-40 w-full'>Applications accepted</h1>
+    <div className="ml-5 mt-3 flex w-40 flex-row border-l-2 ">
+      <h1 className="ml-5 w-full pt-3 text-center text-xl font-semibold tracking-widest text-main sm:w-52 sm:pt-4 sm:text-3xl">
+        {loader ? (
+          <div className="ml-0 w-full text-center text-[1.125rem] font-normal tracking-wider text-main sm:w-40 sm:pt-1 sm:text-base">
+            loading...
+          </div>
+        ) : (
+          
+          <div className="w-20">
+            <CountUp start={0} end={Counter}
+            />
+          </div>
+        )}
+      </h1>
+      
+      <h1 className="w-full text-center text-[0.5rem] sm:pt-[10px] tracking-wider text-main sm:ml-4 sm:text-base">
+        {loader ? " " : "Accepted Applications "}
+      </h1>
     </div>
   );
 };

453d41ca7356f499be90f5060bdae9318d1cdec7
Ubaid <rubaid690@gmail.com>
2023-05-22 09:22:19 +0500

    Updated Application Counter

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -20,25 +20,27 @@ const Counter = () => {
     }
   };
   return (
-    <div className="ml-5 mt-3 flex w-40 flex-row border-l-2 ">
-      <h1 className="ml-5 w-full pt-3 text-center text-xl font-semibold tracking-widest text-main sm:w-52 sm:pt-4 sm:text-3xl">
-        {loader ? (
-          <div className="ml-0 w-full text-center text-[1.125rem] font-normal tracking-wider text-main sm:w-40 sm:pt-1 sm:text-base">
-            loading...
-          </div>
-        ) : (
-          
-          <div className="w-20">
-            <CountUp start={0} end={Counter}
-            />
-          </div>
-        )}
-      </h1>
-      
-      <h1 className="w-full text-center text-[0.5rem] sm:pt-[10px] tracking-wider text-main sm:ml-4 sm:text-base">
-        {loader ? " " : "Accepted Applications "}
-      </h1>
-    </div>
+    <>
+      {Counter >= 100 ? (
+        <div className="ml-5 mt-3 flex w-40 flex-row border-l-2 ">
+          <h1 className="ml-5 w-full pt-3 text-center text-xl font-semibold tracking-widest text-main sm:w-52 sm:pt-4 sm:text-3xl">
+            {loader ? (
+              " "
+            ) : (
+              <div className="w-20">
+                <CountUp start={0} end={Counter} />
+              </div>
+            )}
+          </h1>
+
+          <h1 className="w-full text-center text-[0.5rem] tracking-wider text-main sm:ml-4 sm:pt-[10px] sm:text-base">
+            {loader ? " " : "Accepted Applications "}
+          </h1>
+        </div>
+      ) : (
+        " "
+      )}
+    </>
   );
 };
 

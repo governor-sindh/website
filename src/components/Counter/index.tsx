d50ff9ef930412512ddfb67e691e2211dd7175ee
hamzaadil56 <hamzaadil56@gmail.com>
2023-05-29 19:36:10 +0500

    changes

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,5 +1,3 @@
-import Number from "./Number";
-
 export const Counter = async () => {
   const response = await fetch("http://localhost:3000/api/counter", {
     cache: "no-cache",
@@ -12,7 +10,6 @@ export const Counter = async () => {
         <div className="mt-4 flex w-full flex-col items-center sm:mt-0">
           <div className="flex-col text-center text-xl font-semibold tracking-widest text-main sm:text-3xl">
             <div className="w-36">{data.counter}</div>
-            {/* <Number noOfApplications={counter.counter} /> */}
           </div>
 
           <div className="text-center text-xs tracking-wider text-main sm:mb-0 sm:text-sm">

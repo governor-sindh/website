93ecc92dbdc128760172d6e5d0c3af3d3fd35410
hamzaadil56 <hamzaadil56@gmail.com>
2023-05-29 12:52:35 +0500

    changed counter api

--- src/app/api/counter/route.ts
+++ src/app/api/counter/route.ts
@@ -1,29 +1,26 @@
 import { kv } from "@vercel/kv";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
+export async function GET() {
   try {
     // Fetch the current counter value from KV or initialize it as 0
     const currentValue: any = await kv.get("counter");
 
-    // Increment the counter value by 1
-    const newCounter = currentValue + 1;
-
-    // Store the updated counter value in KV
-    await kv.set("counter", newCounter);
-
-    // return new Response(JSON.stringify({ counter: newCounter }), {
-    //   headers: { "Content-Type": "application/json" },
-    // });
-
+    if (!currentValue) {
+      throw new Error("Failed to update counter");
+    }
     return NextResponse.json({
-      counter: newCounter,
+      counter: currentValue,
     });
-  } catch (error) {
+  } catch (error: any) {
     // console.error("Error:", error);
-    return new Response(JSON.stringify({ error: "Failed to update counter" }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }

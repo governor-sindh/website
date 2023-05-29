fea8f41b363ba938866c96e53b4d6032bc9fb970
hamzaadil56 <hamzaadil56@gmail.com>
2023-05-29 19:26:10 +0500

    Changes in Counter

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,42 +1,23 @@
-"use client";
-import { useEffect, useState } from "react";
-import CountUp from "react-countup";
+import Number from "./Number";
 
-export const Counter = () => {
-  const [counter, setCounter] = useState(0);
-  const [loader, setLoader] = useState(true);
-
-  useEffect(() => {
-    fetchCounterValue();
-  }, []);
-
-  const fetchCounterValue = async () => {
-    try {
-      const response = await fetch("/api/counter");
-      const data = await response.json();
-
-      setCounter(data.counter);
-    } catch (err) {
-    } finally {
-      setLoader(false);
-    }
-  };
+export const Counter = async () => {
+  const response = await fetch("http://localhost:3000/api/counter", {
+    cache: "no-cache",
+  });
+  const data = await response.json();
 
   return (
     <>
-      {counter && !loader ? (
-        <div className="flex w-full flex-col items-center mt-4 sm:mt-0">
-          <>
-            <div className="flex-col text-center text-xl font-semibold tracking-widest text-main sm:text-3xl">
-              <div className="w-36">
-                <CountUp start={0} end={counter} />
-              </div>
-            </div>
+      {data.counter !== 0 ? (
+        <div className="mt-4 flex w-full flex-col items-center sm:mt-0">
+          <div className="flex-col text-center text-xl font-semibold tracking-widest text-main sm:text-3xl">
+            <div className="w-36">{data.counter}</div>
+            {/* <Number noOfApplications={counter.counter} /> */}
+          </div>
 
-            <div className="text-center text-xs tracking-wider text-main sm:mb-0 sm:text-sm">
-              Accepted Applications
-            </div>
-          </>
+          <div className="text-center text-xs tracking-wider text-main sm:mb-0 sm:text-sm">
+            Accepted Applications
+          </div>
         </div>
       ) : null}
     </>

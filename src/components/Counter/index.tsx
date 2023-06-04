c624dd90f42feb14e327f6fd9cb688585fdc8c32
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-06-04 17:10:51 +0500

    counter updated

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -12,7 +12,7 @@ export const Counter = async () => {
   return (
     <div className="mt-4 flex w-full flex-col items-center sm:mt-0">
       <div className="flex-col text-center text-xl tracking-widest text-main sm:text-3xl">
-        <div className="w-36 font-extrabold">176,103</div>
+        <div className="w-36 font-extrabold">228,271</div>
       </div>
       <div className="text-center text-xs tracking-wider text-main sm:mb-0 sm:text-sm">
         Accepted Applications

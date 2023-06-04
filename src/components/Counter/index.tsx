c30cb4ae3ad116e9911a1f9a1418ccc9216f1599
Adil Altaf <62209503+adil-innovation-lab@users.noreply.github.com>
2023-06-03 21:37:37 -0400

    Updating Counter

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -12,7 +12,7 @@ export const Counter = async () => {
   return (
     <div className="mt-4 flex w-full flex-col items-center sm:mt-0">
       <div className="flex-col text-center text-xl tracking-widest text-main sm:text-3xl">
-        <div className="w-36 font-extrabold">228,271</div>
+        <div className="w-36 font-extrabold">207,053</div>
       </div>
       <div className="text-center text-xs tracking-wider text-main sm:mb-0 sm:text-sm">
         Accepted Applications

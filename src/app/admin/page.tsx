bd64d68c182cb5c6814f03a7ed97301d08ce3c67
Tanzeel Tasleem <tanzeeltasleem648@gmail.com>
2023-06-12 15:29:05 +0500

    add: admin page authentication using amplify & cognito

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { Authenticator } from '@aws-amplify/ui-react'
+import React from 'react'
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import awsConfig from '@/config/config';
+
+const AdminPage = () => {
+    Amplify.configure(awsConfig);
+    return (
+        <div className='mt-16'>
+            <Authenticator loginMechanisms={["email", "username"]} hideSignUp>
+                {({ signOut, user }) => (
+                    <main>
+                        <h1>Hello {user?.username}</h1>
+                        <button onClick={signOut}>Sign out</button>
+                    </main>
+                )}
+            </Authenticator>
+
+        </div>
+    )
+}
+
+export default AdminPage

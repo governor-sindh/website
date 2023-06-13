4c163581f3878afc1e1449c79e7e96c0e4b1dc90
uzairbangee <uzairbangee@gmail.com>
2023-06-13 16:38:56 +0500

    feat: delete admin page data

--- src/config/credentials.ts
+++
@@ -1,7 +0,0 @@
-export const Credentials = {
-    cognito: {
-        region: process.env.NEXT_PUBLIC_REGION,
-        userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
-        userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_WEB_CLIENT_ID,
-    }
-};

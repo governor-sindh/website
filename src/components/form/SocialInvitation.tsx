d7227f76aab485363b23f89d85be40f955d0a3c9
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-06-22 02:44:17 +0500

    stepper added

--- src/components/form/SocialInvitation.tsx
+++ src/components/form/SocialInvitation.tsx
@@ -71,74 +71,76 @@ export default function SocialInvitation({
   }
 
   return (
-    <div className="z-10 mx-4 my-10 flex w-full max-w-2xl flex-col items-center justify-center gap-5 rounded bg-opacity-30 px-4 py-8 text-black shadow-lg backdrop-blur-3xl md:mx-14 md:p-16">
-      <h2
-        className="text-center text-xl text-main xs:text-3xl"
-        style={poppins.style}
-      >
-        Before continuing to the application please subscribe on these social
-        media platforms.{" "}
-      </h2>
-
-      <div className="my-5 flex gap-3  md:text-sm">
-        {socialLinks.map((item) =>
-          // @ts-ignore
-          socialSuccess?.[item.platform] ? (
-            <div
-              key={item.id}
-              className="flex h-8 w-8 items-center justify-center rounded-full bg-green-500 text-white"
-            >
-              <GiCheckMark size={14} />
-            </div>
-          ) : (
-            <div
-              key={item.id}
-              onClick={() => OpenSocial(item.link, item.platform)}
-              className={`flex h-8 w-8 cursor-pointer items-center justify-center ${item.class} rounded-full text-white`}
-            >
-              {item.icon}
-            </div>
-          )
-        )}
-      </div>
-
-      {socialSuccess.facebook &&
-      socialSuccess.youtube &&
-      socialSuccess.instagram &&
-      socialSuccess.twitter ? (
-        <button
-          onClick={checkSubscription}
-          className="w-full bg-main py-4 text-center font-semibold tracking-widest text-white transition-all hover:translate-y-1 disabled:opacity-60 disabled:hover:cursor-not-allowed xs:w-52"
+    <>
+      <div className="z-10 mx-4 my-10 flex w-full max-w-2xl flex-col items-center justify-center gap-5 rounded bg-opacity-30 px-4 py-8 text-black shadow-lg backdrop-blur-3xl md:mx-14 md:p-16">
+        <h2
+          className="text-center text-xl text-main xs:text-3xl"
           style={poppins.style}
         >
-          CONTINUE
-        </button>
-      ) : (
-        <div className="group relative mt-4">
-          <div className="popover absolute bottom-16 right-2 z-10 rounded-lg border border-gray-400 bg-white px-4 py-2 text-sm text-gray-500 opacity-0 shadow-sm transition-opacity duration-300 group-hover:opacity-100">
-            <p>Click the links above first.</p>
-          </div>
+          Before continuing to the application please subscribe on these social
+          media platforms.{" "}
+        </h2>
+
+        <div className="mt-5 flex gap-3  md:text-sm">
+          {socialLinks.map((item) =>
+            // @ts-ignore
+            socialSuccess?.[item.platform] ? (
+              <div
+                key={item.id}
+                className="flex h-8 w-8 items-center justify-center rounded-full bg-green-500 text-white"
+              >
+                <GiCheckMark size={14} />
+              </div>
+            ) : (
+              <div
+                key={item.id}
+                onClick={() => OpenSocial(item.link, item.platform)}
+                className={`flex h-8 w-8 cursor-pointer items-center justify-center ${item.class} rounded-full text-white`}
+              >
+                {item.icon}
+              </div>
+            )
+          )}
+        </div>
+
+        {socialSuccess.facebook &&
+        socialSuccess.youtube &&
+        socialSuccess.instagram &&
+        socialSuccess.twitter ? (
           <button
-            disabled
+            onClick={checkSubscription}
             className="w-full bg-main py-4 text-center font-semibold tracking-widest text-white transition-all hover:translate-y-1 disabled:opacity-60 disabled:hover:cursor-not-allowed xs:w-52"
             style={poppins.style}
           >
             CONTINUE
           </button>
-        </div>
-      )}
-      <p className="text-center">
-        Already applied?{" "}
-        <Link className="text-blue-400 underline" href={"/admit-card"}>
-          Get Admit Card
-        </Link>
-      </p>
-
-      {error && (
-        <p className="text-center font-semibold text-red-600">
-          Please follow all social links
+        ) : (
+          <div className="group relative w-full xs:w-52">
+            <div className="popover bottom-16 right-[6.8rem] z-10 rounded-lg bg-white px-4 py-2 text-sm text-red-500 opacity-0 shadow-sm transition-opacity duration-300 group-hover:opacity-100 xs:right-2">
+              <p>Click the links above first.</p>
+            </div>
+            <button
+              disabled
+              className="w-full bg-main py-4 text-center font-semibold tracking-widest text-white transition-all hover:translate-y-1 disabled:opacity-60 disabled:hover:cursor-not-allowed xs:w-52"
+              style={poppins.style}
+            >
+              CONTINUE
+            </button>
+          </div>
+        )}
+        <p className="text-center">
+          Already applied?{" "}
+          <Link className="text-blue-400 underline" href={"/admit-card"}>
+            Get Admit Card
+          </Link>
         </p>
-      )}
-    </div>
+
+        {error && (
+          <p className="text-center font-semibold text-red-600">
+            Please follow all social links
+          </p>
+        )}
+      </div>
+    </>
   );
 }
---
+++ src/components/form/Stepper.tsx
@@ -0,0 +1,51 @@
+import {
+  Step,
+  StepDescription,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  Stepper,
+  useSteps,
+  Box,
+} from "@chakra-ui/react";
+
+const steps = [
+  { title: "First", description: "Contact Info" },
+  { title: "Second", description: "Date & Time" },
+  { title: "Third", description: "Select Rooms" },
+];
+
+function SocialIconStepper() {
+  const { activeStep } = useSteps({
+    index: 1,
+    count: steps.length,
+  });
+
+  return (
+    <Stepper index={activeStep}>
+      {steps.map((step, index) => (
+        <Step key={index}>
+          <StepIndicator>
+            <StepStatus
+              complete={<StepIcon />}
+              incomplete={<StepNumber />}
+              active={<StepNumber />}
+            />
+          </StepIndicator>
+
+          <Box flexShrink="0">
+            <StepTitle>{step.title}</StepTitle>
+            <StepDescription>{step.description}</StepDescription>
+          </Box>
+
+          <StepSeparator />
+        </Step>
+      ))}
+    </Stepper>
+  );
+}
+
+// render(<Example />);

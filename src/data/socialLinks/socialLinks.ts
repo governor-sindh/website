f2472722ce6b9a3cb1448d1f571bbe6757641151
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-08-20 21:30:49 +0500

    added dummy email

--- src/data/socialLinks/socialLinks.ts
+++ src/data/socialLinks/socialLinks.ts
@@ -1,8 +1,8 @@
-const officialEmail = "education@governorsindh.com";
+const officialEmail = "education@governorbalochistan.com";
 const fbLink = "https://www.facebook.com/groups/panaverse";
 const youtubeLink = "https://www.youtube.com/@panaverse";
 const twitterLink = "https://twitter.com/Panaverse_edu";
-const instaLink = "https://instagram.com/KamranTessoriPk";
+const instaLink = "https://instagram.com/";
 const githubLink = "https://github.com/panaverse";
 
 export { officialEmail, fbLink, youtubeLink, twitterLink, instaLink, githubLink };

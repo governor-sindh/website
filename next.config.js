634b9b2b13ef08661350682c601f6938ad2c640f
Daniyal Nagori <daniyalnagori@yahoo.com>
2023-12-26 12:31:59 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  images: {
+    unoptimized: true,
+    minimumCacheTTL: 60,
+  }
 }
 
 module.exports = nextConfig

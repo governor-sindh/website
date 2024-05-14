7bc3696ca59260a230c4395db7e0186e679ae2d3
Daniyal Nagori <daniyalnagori@yahoo.com>
2024-05-14 10:51:48 +0500

    Update index.tsx

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -2,7 +2,7 @@ export const Counter = () => {
   return (
     <div className="mt-4 flex w-full flex-col items-center sm:mt-0">
       <div className="flex-col text-center text-xl tracking-widest text-main sm:text-3xl">
-        <div className="w-40 font-extrabold">14,943</div>
+        <div className="w-40 font-extrabold">5,24,943</div>
       </div>
       <div className="text-center text-xs tracking-wider text-main sm:mb-0 sm:text-sm">
         Accepted Applications
